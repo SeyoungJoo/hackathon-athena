58b646398057fcbc3985c6e85c1bed09277e72af
Seyoung <sellyjphoto@gmail.com>
2022-10-22 18:21:13 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
+import home from "./images/home.png";
+import list from "./images/list.png";
+import camera from "./images/camera.png";
+import heart from "./images/heart.png";
+import user from "./images/user.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <footer className="footer">
-        <a href="">hello</a>
-      </footer>
+      <header className="body"></header>
+
+      <div className="footer">
+        <img src={home} />
+        <img src={list} />
+        <img src={list} className="placeholder_icon" />
+        <img src={camera} className="camera" />
+
+        <img src={heart} />
+        <img src={user} />
+      </div>
     </div>
   );
 }
